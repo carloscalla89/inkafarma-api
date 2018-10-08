ac9c4be84ddac80e971900bb10e94ba8d7c941a4
carlos calla <carlos.calla89@gmail.com>
2018-10-08 01:41:46 -0500

    refactor request swagger

--- src/main/java/com/inkafarma/personsapi/model/PersonRegisterRequest.java
+++ src/main/java/com/inkafarma/personsapi/model/PersonRegisterRequest.java
@@ -15,24 +15,24 @@ import javax.validation.constraints.Pattern;
 public class PersonRegisterRequest {
 
 
-    @ApiModelProperty(notes = "Nombre de la Persona",dataType = "String",example = "Carlos")
+    @ApiModelProperty(notes = "Nombre de la Persona",dataType = "String",example = "Carlos", required = true)
     @NotEmpty
     @NotNull
     private String name;
 
     @NotEmpty
     @NotNull
-    @ApiModelProperty(notes = "Apellido de la Persona",dataType = "String",example = "Calla")
+    @ApiModelProperty(notes = "Apellido de la Persona",dataType = "String",example = "Calla", required = true)
     private String lastName;
 
     @NotNull
-    @ApiModelProperty(notes = "Edad de la Persona",dataType = "String",example = "28")
+    @ApiModelProperty(notes = "Edad de la Persona",dataType = "String",example = "28",required = true)
     private Integer age;
 
     @Pattern(regexp="^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$",
             message="Formato de fecha incorrecto. El formato es YYYY-mm-dd")
     @NotEmpty
     @NotNull
-    @ApiModelProperty(notes = "Fecha de Nacimiento de la Persona",dataType = "String",example = "1989-05-21")
+    @ApiModelProperty(notes = "Fecha de Nacimiento de la Persona",dataType = "String",example = "1989-05-21", required = true)
     private String birthDate;
 }
